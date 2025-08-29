33072d917cc4107222976389c4bf5cf44ab82d52
Alwin-B-UIUX-Dev <201751341+Alwin-B-UIUX-Dev@users.noreply.github.com>
2025-08-29 14:37:59 +0200

    feat: interface entities addresses

--- src/interfaces/entities/addresses/IAddresses.ts
+++ src/interfaces/entities/addresses/IAddresses.ts
@@ -2,32 +2,30 @@ import type { IEntity } from '@/interfaces';
 
 export interface IAddresses extends IEntity {
   // === GETTERS COMPLETS ===
-  getUserId(): string;
-  getType(): string;
+  getUserId(): number;
   getFirstName(): string;
   getLastName(): string;
-  getCompany(): string;
+  getCompany(): string | null;
   getPhone(): string;
   getAddressLine1(): string;
-  getAddressLine2(): string;
+  getAddressLine2(): string | null;
   getCity(): string;
   getPostalCode(): string;
-  getStateRegion(): string;
+  getStateRegion(): string | null;
   getCountry(): string;
   getIsDefault(): boolean;
 
   // === SETTERS COMPLETS ===
-  setUserId(userId: string): this;
-  setType(type: string): this;
+  setUserId(userId: number): this;
   setFirstName(firstName: string): this;
   setLastName(lastName: string): this;
-  setCompany(company: string): this;
+  setCompany(company: string | null): this;
   setPhone(phone: string): this;
   setAddressLine1(addressLine1: string): this;
-  setAddressLine2(addressLine2: string): this;
+  setAddressLine2(addressLine2: string | null): this;
   setCity(city: string): this;
   setPostalCode(postalCode: string): this;
-  setStateRegion(stateRegion: string): this;
+  setStateRegion(stateRegion: string | null): this;
   setCountry(country: string): this;
   setIsDefault(isDefault: boolean): this;
   // === MÉTHODES MÉTIER ===
