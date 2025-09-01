8a39e65e49e82d875ba69b82fcd5ca36eb1eb0e4
Alwin-B-UIUX-Dev <201751341+Alwin-B-UIUX-Dev@users.noreply.github.com>
2025-09-01 15:32:34 +0200

    feat: reposotory orders

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -1,3 +1,4 @@
 export * from '@/repositories/addresses/index';
 export * from '@/repositories/order-items/index';
+export * from '@/repositories/orders/index';
 export * from '@/repositories/product/index';
