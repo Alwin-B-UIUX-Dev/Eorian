cd6f065a8f0f808fa401e7f169253a1c349e79e9
Alwin-B-UIUX-Dev <201751341+Alwin-B-UIUX-Dev@users.noreply.github.com>
2025-09-16 11:16:59 +0200

    feat: create cart-items controllers interface

--- src/interfaces/controllers/index.ts
+++ src/interfaces/controllers/index.ts
@@ -1,4 +1,5 @@
 export * from '@/interfaces/controllers/auth/index';
+export * from '@/interfaces/controllers/cart-items/index';
 export * from '@/interfaces/controllers/products/index';
 export * from '@/interfaces/controllers/tax-rates/index';
 export * from '@/interfaces/controllers/user/index';
