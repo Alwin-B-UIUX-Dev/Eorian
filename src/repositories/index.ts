f840e07459489f64a4627624b1bf1db17461412b
Alwin-B-UIUX-Dev <201751341+Alwin-B-UIUX-Dev@users.noreply.github.com>
2025-09-16 11:11:10 +0200

    feat: create cart-items repository

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -1,3 +1,4 @@
+export * from '@/repositories/cart-items/index';
 export * from '@/repositories/products/index';
 export * from '@/repositories/tax-rates/index';
 export * from '@/repositories/user/index';
