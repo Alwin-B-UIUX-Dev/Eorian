fbc1efe89082302c79cd36445a1d275e4f0dc9af
Alwin-B-UIUX-Dev <201751341+Alwin-B-UIUX-Dev@users.noreply.github.com>
2025-09-16 11:18:27 +0200

    feat: create cart-items routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,5 @@
 export * from '@/routes/AuthRoutes';
+export * from '@/routes/CartItemRoutes';
 export * from '@/routes/ProductRoutes';
 export * from '@/routes/TaxeRateRoutes';
 export * from '@/routes/UserRoleRoutes';
