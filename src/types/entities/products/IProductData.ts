2ce1afa572db0df69d9beba20cc3f19c5b912ca5
Alwin-B-UIUX-Dev <201751341+Alwin-B-UIUX-Dev@users.noreply.github.com>
2025-09-16 09:42:04 +0200

    feat: add product type

---
+++ src/types/entities/products/IProductData.ts
@@ -0,0 +1,25 @@
+import type {
+  IBaseEntityData,
+  PartialWithoutSystemFieldsType,
+  WithoutSystemFieldsType
+} from '@/types';
+
+export interface IProductData extends IBaseEntityData {
+  name: string;
+  slug: string;
+  sku: string | null;
+  shortDescription: string | null;
+  description: string | null;
+  priceCents: number;
+  taxRateId: number;
+  stockQuantity: number;
+  lowStockThreshold: number;
+  manageStock: boolean;
+  metaTitle: string | null;
+  metaDescription: string | null;
+  isActive: boolean;
+  createdBy: number;
+}
+
+export type CreateProductData = WithoutSystemFieldsType<IProductData>;
+export type UpdateProductData = PartialWithoutSystemFieldsType<IProductData>;
