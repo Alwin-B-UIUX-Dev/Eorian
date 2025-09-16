080455873bb86260476eabf50c1fe8bf483ea101
Alwin-B-UIUX-Dev <201751341+Alwin-B-UIUX-Dev@users.noreply.github.com>
2025-09-16 10:07:04 +0200

    feat: add product controllers

--- src/interfaces/controllers/index.ts
+++ src/interfaces/controllers/index.ts
@@ -1,3 +1,4 @@
 export * from '@/interfaces/controllers/auth/index';
+export * from '@/interfaces/controllers/products/index';
 export * from '@/interfaces/controllers/tax-rates/index';
 export * from '@/interfaces/controllers/user/index';
---
+++ src/interfaces/controllers/products/index.ts
@@ -0,0 +1,1 @@
+export * from '@/interfaces/controllers/products/IProductController';
