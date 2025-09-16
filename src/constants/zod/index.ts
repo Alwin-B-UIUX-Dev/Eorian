217a5251ea62009f34b09973a3e30e0aeedecb98
Alwin-B-UIUX-Dev <201751341+Alwin-B-UIUX-Dev@users.noreply.github.com>
2025-09-16 09:56:16 +0200

    feat: add product zod index

--- src/constants/zod/index.ts
+++ src/constants/zod/index.ts
@@ -1,4 +1,5 @@
 export * from '@/constants/zod/AuthConstants';
+export * from '@/constants/zod/ProductConstants';
 export * from '@/constants/zod/TaxeRateConstants';
 export * from '@/constants/zod/UserConstants';
 export * from '@/constants/zod/UserRoleConstants';
