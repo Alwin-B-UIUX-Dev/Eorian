e001cb52273d173790b9736b74aa8b84deea7fb1
Alwin-B-UIUX-Dev <201751341+Alwin-B-UIUX-Dev@users.noreply.github.com>
2025-09-17 11:12:51 +0200

    feat: create order & order-item entity

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -1,6 +1,7 @@
 export * from '@/entities/addresses/index';
 export * from '@/entities/BaseEntity';
 export * from '@/entities/cart-items/index';
+export * from '@/entities/orders/index';
 export * from '@/entities/products/index';
 export * from '@/entities/tax-rates/index';
 export * from '@/entities/user/index';
